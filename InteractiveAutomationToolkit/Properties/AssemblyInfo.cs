7bff911c369d53e1f0a9b9ca567f10672ce16766
Thomas Cauwelier <thomas.cauwelier@skyline.be>
2022-04-13 15:20:11 +0200

    Generate NuGet package
    Change-Id: I16dae25a6948dc9fe407fbf9459e0b204bd42a76

--- InteractiveAutomationToolkit/Properties/AssemblyInfo.cs
+++ InteractiveAutomationToolkit/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InteractiveAutomationToolkit")]
-[assembly: AssemblyDescription("Framework used to create Interactive Automation Scripts for DataMiner")]
+[assembly: AssemblyTitle("SLC.CLib.IASToolkit")]
+[assembly: AssemblyDescription("Framework used to create Interactive Automation Scripts in DataMiner for DMA version larger than 10.1.5")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skyline Communications")]
 [assembly: AssemblyProduct("InteractiveAutomationToolkit")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.4")]
-[assembly: AssemblyFileVersion("1.0.6.4")]
+[assembly: AssemblyVersion("1.0.6.5")]
+[assembly: AssemblyFileVersion("1.0.6.5")]
 [assembly: NeutralResourcesLanguage("")]
 
