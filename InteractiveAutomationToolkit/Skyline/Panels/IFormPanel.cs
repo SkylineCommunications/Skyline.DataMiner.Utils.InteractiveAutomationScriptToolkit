7d149f959fbb98b3cc10c69df7b99ec0a7fa30ce
Thomas Cauwelier <thomas.cauwelier@skyline.be>
2022-08-01 18:13:34 +0200

    Add FormPanel
    Change-Id: Ica73a850cada67f98af92f9e0c2a40c5c6047898

---
+++ InteractiveAutomationToolkit/Skyline/Panels/IFormPanel.cs
@@ -0,0 +1,15 @@
+﻿namespace Skyline.DataMiner.InteractiveAutomationToolkit
+{
+	public interface IFormPanel : IPanel
+	{
+		void Add(string label, IComponent component);
+
+		void Add(ILabel label, IComponent component);
+
+		void Add(string label, IWidget widget, int span);
+
+		void Add(ILabel label, IWidget widget, int span);
+
+		void Remove(IComponent component);
+	}
+}
