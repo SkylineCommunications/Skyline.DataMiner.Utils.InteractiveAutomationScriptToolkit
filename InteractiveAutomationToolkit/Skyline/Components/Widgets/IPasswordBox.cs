df41c53184f0a2c71d0440116b75b6db99363d59
Thomas Cauwelier <thomas.cauwelier@skyline.be>
2023-01-23 16:30:24 +0100

    Improve xmldoc for placeholder and tooltip property
    Change-Id: If106a2b941e3a55c80e36645b0a7cff8e6632480

--- InteractiveAutomationToolkit/Skyline/Components/Widgets/IPasswordBox.cs
+++ InteractiveAutomationToolkit/Skyline/Components/Widgets/IPasswordBox.cs
@@ -26,12 +26,15 @@ namespace Skyline.DataMiner.Utils.InteractiveAutomationToolkit
 		/// <summary>
 		///     Gets or sets the text that should be displayed as a placeholder.
 		/// </summary>
+		/// <remarks>This property only works for web compliant scripts or when launched from a DataMiner web app.</remarks>
 		/// <remarks>Available from DataMiner Feature Release 10.0.5 and Main Release 10.1.0 onwards.</remarks>
 		string PlaceHolder { get; set; }
 
 		/// <summary>
 		///     Gets or sets the tooltip.
 		/// </summary>
+		/// <remarks>This property only works for web compliant scripts or when launched from a DataMiner web app.</remarks>
+		/// <remarks>Available from DataMiner Feature Release 10.0.8 and Main Release 10.1.0 onwards.</remarks>
 		string Tooltip { get; set; }
 	}
 }
--- InteractiveAutomationToolkit/Skyline/Components/Widgets/ITime.cs
+++ InteractiveAutomationToolkit/Skyline/Components/Widgets/ITime.cs
@@ -65,6 +65,8 @@ namespace Skyline.DataMiner.Utils.InteractiveAutomationToolkit
 		/// <summary>
 		///     Gets or sets the tooltip.
 		/// </summary>
+		/// <remarks>This property only works for web compliant scripts or when launched from a DataMiner web app.</remarks>
+		/// <remarks>Available from DataMiner Feature Release 10.0.8 and Main Release 10.1.0 onwards.</remarks>
 		string Tooltip { get; set; }
 
 		/// <summary>
