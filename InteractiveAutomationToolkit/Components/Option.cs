228b39bedb7ebf32f9657d005655b808af7d09af
Tom Waterbley <tom.waterbley@skyline.be>
2025-01-20 16:31:29 +0100

    Support empty Option<T>

--- InteractiveAutomationToolkit/Components/Option.cs
+++ InteractiveAutomationToolkit/Components/Option.cs
@@ -1,17 +1,18 @@
 ﻿namespace Skyline.DataMiner.Utils.InteractiveAutomationScript
 {
 	using System;
+	using System.Collections.Generic;
 
-	public sealed class Option<T> : IEquatable<Option<T>>
+	public readonly struct Option<T> : IEquatable<Option<T>>
 	{
+		public static readonly Option<T> Empty = default;
+
 		public Option(T value) : this(Convert.ToString(value), value)
 		{
 		}
 
 		public Option(string displayedValue, T value)
 		{
-			if (String.IsNullOrWhiteSpace(displayedValue)) throw new ArgumentException($"Displayed value cannot be null or whitespace");
-
 			DisplayValue = displayedValue;
 			Value = value;
 		}
@@ -19,6 +20,8 @@
 		public string DisplayValue { get; }
 
 		public T Value { get; }
+
+		public bool IsEmpty => Equals(Empty);
 
 		public static bool operator ==(Option<T> left, Option<T> right)
 		{
@@ -32,27 +35,39 @@
 
 		public override bool Equals(object obj)
 		{
-			if (!(obj is Option<T> other)) return false;
-			if (!String.Equals(DisplayValue, other.DisplayValue)) return false;
-			return true;
+			return obj is Option<T> option && Equals(option);
 		}
 
 		public bool Equals(Option<T> other)
 		{
-			if (!String.Equals(DisplayValue, other.DisplayValue)) return false;
-			return true;
+			var thisDisplay = String.IsNullOrEmpty(DisplayValue) ? String.Empty : DisplayValue;
+			var otherDisplay = String.IsNullOrEmpty(other.DisplayValue) ? String.Empty : other.DisplayValue;
+
+			return String.Equals(thisDisplay, otherDisplay) &&
+				EqualityComparer<T>.Default.Equals(Value, other.Value);
 		}
 
 		public override int GetHashCode()
 		{
+			var normalizedDisplay = String.IsNullOrEmpty(DisplayValue) ? String.Empty : DisplayValue;
+
 			int hashCode = 11;
-			hashCode ^= 13 * DisplayValue.GetHashCode();
+			hashCode ^= 13 * normalizedDisplay.GetHashCode();
+			hashCode ^= 13 * (Value != null ? Value.GetHashCode() : 0);
 			return hashCode;
 		}
 
 		public override string ToString()
 		{
 			return $"{DisplayValue} => {Value}";
+		}
+	}
+
+	public static class Option
+	{
+		public static Option<T> Create<T>(string displayValue, T value)
+		{
+			return new Option<T>(displayValue, value);
 		}
 	}
 }
