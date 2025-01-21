78542c5ac3cde9ee26e2af1dcee31b18041cdd89
Thomas Remmery <thomas.remmery@skyline.be>
2025-01-21 15:59:10 +0100

    Add content file to automatically add it to every project referencing the IAS toolkit.

---
+++ InteractiveAutomationToolkit/content/Engine.ShowUI.cs
@@ -0,0 +1,3 @@
+// This file is automatically added to every project referencing the IAS Toolkit.
+// Is makes sure that DM detects this project a an interactive automation script based on the following comment.
+// Engine.ShowUI();
