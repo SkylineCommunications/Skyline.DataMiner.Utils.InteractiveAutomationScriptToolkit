dbb35f9fbc9b48ff42be0fd24f2ca085c79d3667
VictorScherpereell <victor.scherpereel@skyline.be>
2025-01-23 11:38:03 +0100

    Handling InteractiveUserDetachedException separately

--- InteractiveAutomationToolkit/Dialogs/Dialog.cs
+++ InteractiveAutomationToolkit/Dialogs/Dialog.cs
@@ -6,6 +6,7 @@
 	using System.Linq;
 
 	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Net.Exceptions;
 
 	/// <summary>
 	///     A dialog represents a single window that can be shown.
@@ -612,7 +613,11 @@
 			{
 				uiResults = new WrappedUIResults(Engine.ShowUI(uiBuilder));
 			}
-			catch (Exception e)
+			catch(InteractiveUserDetachedException)
+			{
+				throw;
+			}
+			catch (DataMinerException e)
 			{
 				throw new InvalidOperationException($"{nameof(IEngine)}.{nameof(Engine.ShowUI)} failed with {nameof(UIBuilder)} argument {uiBuilder}", e);
 			}
