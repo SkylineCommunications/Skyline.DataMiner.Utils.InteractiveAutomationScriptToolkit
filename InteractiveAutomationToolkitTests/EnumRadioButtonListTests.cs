393596a71bb0498693a97619c163e6a6e9d7c42e
Thomas Remmery <thomas.remmery@skyline.be>
2025-02-07 09:39:22 +0100

    Added unittests for conversion constructors of enum widgets

--- InteractiveAutomationToolkitTests/EnumRadioButtonListTests.cs
+++ InteractiveAutomationToolkitTests/EnumRadioButtonListTests.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+    using System;
     using System.Linq;
 
     [TestClass]
@@ -54,6 +55,71 @@
             Assert.AreEqual(DefaultOption.None, radioButtonList.Selected);
             Assert.AreEqual(0, radioButtonList.Options.Count());
             Assert.IsNull(radioButtonList.SelectedOption);
+        }
+
+        [TestMethod]
+        public void ConversionConstructorTest()
+        {
+            var radioButtonList = new EnumRadioButtonList<DefaultOption>(ConversionMethod);
+            Assert.AreEqual(DefaultOption.None, radioButtonList.Selected);
+            Assert.AreEqual(4, radioButtonList.Options.Count());
+
+            Assert.IsNull(radioButtonList.SelectedOption);
+
+            radioButtonList.Selected = DefaultOption.None;
+            Assert.AreEqual("No option", radioButtonList.SelectedOption.DisplayValue);
+
+            radioButtonList.Selected = DefaultOption.Option1;
+            Assert.AreEqual("The first option", radioButtonList.SelectedOption.DisplayValue);
+
+            radioButtonList.Selected = DefaultOption.Option2;
+            Assert.AreEqual("The second option", radioButtonList.SelectedOption.DisplayValue);
+
+            radioButtonList.Selected = DefaultOption.Option3;
+            Assert.AreEqual("The last and final option", radioButtonList.SelectedOption.DisplayValue);
+        }
+
+        [TestMethod]
+        public void ConversionExcludedConstructorTest()
+        {
+            var radioButtonList = new EnumRadioButtonList<DefaultOption>(ConversionMethod, new[] { DefaultOption.None });
+            Assert.AreEqual(DefaultOption.None, radioButtonList.Selected);
+            Assert.AreEqual(3, radioButtonList.Options.Count());
+        }
+
+        [TestMethod]
+        public void ConversionExcludeAllConstructorTest()
+        {
+            var radioButtonList = new EnumRadioButtonList<DefaultOption>(ConversionMethod, new[] { DefaultOption.None, DefaultOption.Option1, DefaultOption.Option2, DefaultOption.Option3 });
+            Assert.AreEqual(DefaultOption.None, radioButtonList.Selected);
+            Assert.AreEqual(0, radioButtonList.Options.Count());
+            Assert.IsNull(radioButtonList.SelectedOption);
+        }
+
+        [TestMethod]
+        public void ConversionNullConstructorTest()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                new EnumRadioButtonList<DefaultOption>(null, new DefaultOption[0]);
+            });
+        }
+
+        private static string ConversionMethod(DefaultOption option)
+        {
+            switch (option)
+            {
+                case DefaultOption.None:
+                    return "No option";
+                case DefaultOption.Option1:
+                    return "The first option";
+                case DefaultOption.Option2:
+                    return "The second option";
+                case DefaultOption.Option3:
+                    return "The last and final option";
+                default:
+                    return "Unknown option";
+            }
         }
     }
 }
