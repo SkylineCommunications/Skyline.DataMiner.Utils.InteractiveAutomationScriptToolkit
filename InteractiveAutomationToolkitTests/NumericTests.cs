d2ffac5b193aca21271f84fcdec29cb62089f3c1
Thomas Remmery <thomas.remmery@skyline.be>
2025-09-09 16:01:30 +0200

    Add unittest covering issue where numeric would not report its changes.

---
+++ InteractiveAutomationToolkitTests/NumericTests.cs
@@ -0,0 +1,86 @@
+﻿namespace InteractiveAutomationToolkitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+    using System;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public class NumericTests
+    {
+        [TestMethod]
+        public void OnChange_InvalidString()
+        {
+            var numeric = new Skyline.DataMiner.Utils.InteractiveAutomationScript.Numeric(10);
+
+            List<Numeric.NumericChangedEventArgs> changedResults = new List<Numeric.NumericChangedEventArgs>();
+            numeric.Changed += (s, e) => changedResults.Add(e);
+
+            var mockedUiResults = new Mock<IUIResults>();
+            mockedUiResults.Setup(x => x.GetString(It.IsAny<string>())).Returns("hello world");
+
+            numeric.LoadResult(mockedUiResults.Object);
+            numeric.RaiseResultEvents();
+
+            Assert.AreEqual(0, changedResults.Count);
+        }
+
+        [TestMethod]
+        public void OnChange_NoDecimals()
+        {
+            var numeric = new Skyline.DataMiner.Utils.InteractiveAutomationScript.Numeric(10);
+
+            List<Numeric.NumericChangedEventArgs> changedResults = new List<Numeric.NumericChangedEventArgs>();
+            numeric.Changed += (s, e) => changedResults.Add(e);
+
+            var mockedUiResults = new Mock<IUIResults>();
+            mockedUiResults.Setup(x => x.GetString(It.IsAny<string>())).Returns("10.01");
+
+            numeric.LoadResult(mockedUiResults.Object);
+            numeric.RaiseResultEvents();
+
+            Assert.AreEqual(1, changedResults.Count);
+        }
+
+        [TestMethod]
+        public void OnChange_3Decimals()
+        {
+            var numeric = new Skyline.DataMiner.Utils.InteractiveAutomationScript.Numeric(10)
+            {
+                Decimals = 3,
+            };
+
+            List<Numeric.NumericChangedEventArgs> changedResults = new List<Numeric.NumericChangedEventArgs>();
+            numeric.Changed += (s, e) => changedResults.Add(e);
+
+            var mockedUiResults = new Mock<IUIResults>();
+            mockedUiResults.Setup(x => x.GetString(It.IsAny<string>())).Returns("10.01");
+
+            numeric.LoadResult(mockedUiResults.Object);
+            numeric.RaiseResultEvents();
+
+            Assert.AreEqual(1, changedResults.Count);
+        }
+
+        [TestMethod]
+        public void OnChange_StepSize()
+        {
+            var numeric = new Skyline.DataMiner.Utils.InteractiveAutomationScript.Numeric(10)
+            {
+                StepSize = 3,
+            };
+
+            List<Numeric.NumericChangedEventArgs> changedResults = new List<Numeric.NumericChangedEventArgs>();
+            numeric.Changed += (s, e) => changedResults.Add(e);
+
+            var mockedUiResults = new Mock<IUIResults>();
+            mockedUiResults.Setup(x => x.GetString(It.IsAny<string>())).Returns("10.01");
+
+            numeric.LoadResult(mockedUiResults.Object);
+            numeric.RaiseResultEvents();
+
+            Assert.AreEqual(1, changedResults.Count);
+        }
+    }
+}
