19df1290d2b8aa4dae0732fa33fe16ecff5e9f88
Thomas Remmery <thomas.remmery@skyline.be>
2025-09-09 15:50:24 +0200

    Compare string values instead of parsed doubles with Decimal margin in Numeric widget.

--- InteractiveAutomationToolkit/Components/Numeric.cs
+++ InteractiveAutomationToolkit/Components/Numeric.cs
@@ -15,6 +15,7 @@
 		private bool changed;
 		private bool focusLost;
 
+		private string previousStringValue = String.Empty;
 		private double previous;
 		private double value;
 
@@ -290,9 +291,11 @@
 				focusLost = wasOnFocusLost;
 			}
 
+			string currentStringValue = uiResults.GetString(this);
+
 			double result;
 			if (!Double.TryParse(
-				uiResults.GetString(this),
+				currentStringValue,
 				NumberStyles.Float,
 				CultureInfo.InvariantCulture,
 				out result))
@@ -300,11 +303,12 @@
 				return;
 			}
 
-			bool isNotEqual = !IsEqualWithinDecimalMargin(result, value);
+			bool isNotEqual = !String.Equals(previousStringValue, currentStringValue);
 			if (isNotEqual && BlockDefinition.WantsOnChange)
 			{
 				changed = true;
 				previous = result;
+				previousStringValue = currentStringValue;
 			}
 
 			Value = result;
@@ -343,11 +347,6 @@
 			{
 				throw new ArgumentException("Infinity is not allowed", "value");
 			}
-		}
-
-		private bool IsEqualWithinDecimalMargin(double a, double b)
-		{
-			return Math.Abs(a - b) < Math.Pow(10, -Decimals);
 		}
 
 		/// <summary>
