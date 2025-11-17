16ca9ba13215317ca32b8cca75b2a8789f213a78
SKYLINE2\ThomasRE <thomas.remmery@skyline.be>
2025-11-17 10:32:32 +0100

    Make margin static readonly.

--- InteractiveAutomationToolkit/Components/Numeric.cs
+++ InteractiveAutomationToolkit/Components/Numeric.cs
@@ -12,6 +12,8 @@
 	/// </summary>
 	public class Numeric : InteractiveWidget, IValidationWidget, IIsReadonlyWidget
 	{
+		private static readonly double epsilon = Math.Pow(10, -12);
+
 		private bool changed;
 		private bool focusLost;
 
@@ -333,7 +335,7 @@
 
 		private bool IsEqualWithinMargin(double a, double b)
 		{
-			return Math.Abs(a - b) < Math.Pow(10, -12);
+			return Math.Abs(a - b) < epsilon;
 		}
 
 		// ReSharper disable once ParameterOnlyUsedForPreconditionCheck.Local
