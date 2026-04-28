5f7ee1a38eee51a8c2a5879ff2996b88c1c0dbbc
Thomas <thomas.remmery@skyline.be>
2026-04-28 11:28:59 +0200

    Fix CoPilot remarks.

--- InteractiveAutomationToolkit/Components/OptionCollection.cs
+++ InteractiveAutomationToolkit/Components/OptionCollection.cs
@@ -52,11 +52,6 @@
 			return rawValueMapping.GetRawValue(option);
 		}
 
-		internal bool TryGetRawValue(Option<T> option, out string rawValue)
-		{
-			return rawValueMapping.TryGetRawValue(option, out rawValue);
-		}
-
 		public void CopyTo(Option<T>[] array, int arrayIndex)
 		{
 			if (array == null) throw new ArgumentNullException("array");
