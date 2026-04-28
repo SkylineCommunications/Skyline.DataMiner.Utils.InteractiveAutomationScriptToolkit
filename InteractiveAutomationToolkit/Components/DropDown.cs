076cde0866e12ec12858a1a06cb94d32e4e78907
Thomas <thomas.remmery@skyline.be>
2026-04-28 11:19:35 +0200

    Fix DropDown filter logic.

--- InteractiveAutomationToolkit/Components/DropDown.cs
+++ InteractiveAutomationToolkit/Components/DropDown.cs
@@ -173,6 +173,8 @@
 		/// <inheritdoc	/>
 		protected internal override void LoadResult(IUIResults uiResults, ILogger logger = null)
 		{
+			base.LoadResult(uiResults, logger);
+
 			var rawSelectedValue = uiResults.GetString(this);
 			logger?.Debug(nameof(DropDown), nameof(LoadResult), $"Raw selected value: {rawSelectedValue}");
 
@@ -195,6 +197,8 @@
 		/// <inheritdoc	/>
 		protected internal override void RaiseResultEvents(ILogger logger = null)
 		{
+			base.RaiseResultEvents(logger);
+
 			if (changed)
 			{
 				logger?.Debug(nameof(DropDown), nameof(RaiseResultEvents), $"OnChange; Selected changed from {previous} to {Selected}");
--- InteractiveAutomationToolkit/Components/Generics/GenericDropDown.cs
+++ InteractiveAutomationToolkit/Components/Generics/GenericDropDown.cs
@@ -243,6 +243,8 @@
 		/// <inheritdoc	/>
 		protected internal override void LoadResult(IUIResults uiResults, ILogger logger = null)
 		{
+			base.LoadResult(uiResults, logger);
+
 			var rawSelectedValue = uiResults.GetString(this);
 			logger?.Debug(nameof(DropDown), nameof(LoadResult), $"Raw selected value: {rawSelectedValue}");
 
@@ -265,6 +267,8 @@
 		/// <inheritdoc	/>
 		protected internal override void RaiseResultEvents(ILogger logger = null)
 		{
+			base.RaiseResultEvents(logger);
+
 			if (changed)
 			{
 				logger?.Debug(nameof(DropDown), nameof(RaiseResultEvents), $"OnChange; Selected changed from {previous?.DisplayValue} to {SelectedOption.DisplayValue}");
